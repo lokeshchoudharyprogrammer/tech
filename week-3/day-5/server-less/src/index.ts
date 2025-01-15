ee95f9cf78f1b795412a78c70f5bcb51b1afb184
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-15 23:14:13 +0530

    hono app and CloudflareWorkers deply

---
+++ week-3/day-5/server-less/src/index.ts
@@ -0,0 +1,39 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+
+		var url=request.url.replace(/^https:\/\/.*?\//gi, '/');
+		console.log((url))
+		
+		if (request.method === "GET") {
+			if(url=="/user"){
+				return new Response(JSON.stringify({name:"John",age:30}),{status:200});
+			}
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
+	},
+};
