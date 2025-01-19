244403b173ab98d5d7eed7650bc2a8c8964a59d0
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-19 23:18:09 +0530

    docker started

---
+++ week-4/day-1/docker.js
@@ -0,0 +1,15 @@
+/**
+ * today i learned about docker
+ * commands
+ * docker ps
+ * docker ps -a
+ * docker run -d -p 80:80 nginx
+ * docker ps -a
+ * docker stop <id>
+ * docker rm <id>
+ * docker images
+ * docker rmi <id>
+ * docker logs <id>
+ * docker exec -it <id> bash
+ * docker build -t <name> .
+ */
