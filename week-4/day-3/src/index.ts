773a4443a041a092b8ccd318789915d9d4d0ff16
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-22 23:00:45 +0530

    docker-compose, push and exec

---
+++ week-4/day-3/src/index.ts
@@ -0,0 +1,36 @@
+
+import { PrismaClient } from "@prisma/client";
+import express,{Request, Response} from "express";
+
+const app = express();
+app.use(express.json());
+
+const client = new PrismaClient();
+
+app.get("/", (req:Request, res:Response) => {
+    res.json({
+        message: "Healthy server"
+    })
+})
+app.post("/", (req:Request, res:Response) => {
+    res.json({
+        message: "Healthy server"
+    })
+})
+
+app.post("/", async (req:Request, res:Response) => {
+    await client.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name
+        }
+    })
+
+    res.json({
+        message: "Done signing up!"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
