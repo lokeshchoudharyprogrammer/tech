7633ab7dbd79bab5f8baebe85bd6cdc245b5832c
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2025-01-27 23:17:32 +0530

    k8s

---
+++ week-5/day-2/k8s.js
@@ -0,0 +1,19 @@
+// today i learn k8s
+
+```
+🗓️ Day-33 Development Journey 🚀  
+
+✅ Explored Kubernetes, starting with:  
+   - **What is Kubernetes?**  
+   - Before & After Kubernetes.  
+✅ Learned key concepts:  
+   - Nodes, kubectl, and Kubernetes API.  
+✅ Hands-on practice:  
+   - Installed kind and kubectl.  
+   - Created a K8s cluster and a Pod.  
+   - Stopped the Pod and explored Kubernetes manifests.  
+
+📌 Progressed with foundational Kubernetes knowledge for real-world applications.  
+
+#Day33 #Development #Kubernetes #Containers
+```
